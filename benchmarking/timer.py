4825313902b887a186ceb653ef4ea0c05f8b465d
timf34 <farrelti@tcd.ie>
2022-06-27 00:15:01 +0100

    changed method name

--- benchmarking/timer.py
+++ benchmarking/timer.py
@@ -13,7 +13,7 @@ class Timer:
     def start(self):
         self.start_time = time.perf_counter()
 
-    def end(self):
+    def stop(self):
         self.end_time = time.perf_counter()
         self.elapsed_time = self.end_time - self.start_time
         self.average_time = self.elapsed_time / self.episodes
