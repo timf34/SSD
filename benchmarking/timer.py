462e9a0682261ebe7a8f5d2bf82946d7d3078484
timf34 <farrelti@tcd.ie>
2022-06-27 00:02:52 +0100

    added Timer

---
+++ benchmarking/timer.py
@@ -0,0 +1,33 @@
+import time
+
+
+class Timer:
+    def __init__(self):
+        self.start_time: int
+        self.end_time: int
+        self.elapsed_time: int
+        self.average_time: int = 0
+        self.steps: int
+        self.episodes: int = 0
+
+    def start(self):
+        self.start_time = time.perf_counter()
+
+    def end(self):
+        self.end_time = time.perf_counter()
+        self.elapsed_time = self.end_time - self.start_time
+        self.average_time = self.elapsed_time / self.episodes
+
+    def update_episodes(self):
+        self.episodes += 1
+
+    def update_individual_step(self):
+        self.steps += 1
+
+    def update_batch_steps(self, steps: int):
+        self.steps += steps
+
+    def print_stats(self):
+        print("Elapsed time:", self.elapsed_time)
+        print("Average time per episode:", self.average_time)
+        print("Average time per step:", self.average_time / self.steps)
