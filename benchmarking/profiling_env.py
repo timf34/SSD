c9a4e697508ab87f6e33c7fd276308fcb2aa25f0
timf34 <farrelti@tcd.ie>
2022-07-06 10:49:44 +0100

    added timer

--- benchmarking/profiling_env.py
+++ benchmarking/profiling_env.py
@@ -5,6 +5,8 @@ import numpy as np
 from social_dilemmas.envs.env_creator import get_env_creator
 from benchmarking.timer import Timer
 
+t = Timer()
+
 
 def run_harvest_env():
     harvest_env = get_env_creator("harvest", 5, {})(0)
@@ -14,11 +16,15 @@ def run_harvest_env():
 
     actions = {}
 
+    t.start()
+
     for _ in range(1000):
         for agent_id in agent_ids:
             actions[agent_id] = np.random.randint(8)
         harvest_env.step(actions)
 
+    t.stop()
+    t.print_stats()
     print("done")
 
 
