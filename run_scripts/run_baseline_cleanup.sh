d6076bce14af65622d138f0e86245b4a770e9526
timf34 <66926418+timf34@users.noreply.github.com>
2022-08-19 14:36:13 +0100

    Changed num_workers to 2 for Colab; reduced num_samples

--- run_scripts/run_baseline_cleanup.sh
+++ run_scripts/run_baseline_cleanup.sh
@@ -5,7 +5,7 @@ python train.py \
 --model baseline \
 --algorithm PPO \
 --num_agents 5 \
---num_workers 6 \
+--num_workers 2 \
 --rollout_fragment_length 1000 \
 --num_envs_per_worker 16 \
 --stop_at_timesteps_total $((500 * 10 ** 6)) \
@@ -14,7 +14,7 @@ python train.py \
 --gpus_per_worker 0 \
 --gpus_for_driver 1 \
 --cpus_for_driver 0 \
---num_samples 5 \
+--num_samples 1 \
 --entropy_coeff 0.00176 \
 --lr_schedule_steps 0 20000000 \
 --lr_schedule_weights .00126 .000012
